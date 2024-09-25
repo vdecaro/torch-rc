86ff5832a3af8f0eb3dffc744afb6f89728d62a3
vdecaro <43961074+vdecaro@users.noreply.github.com>
2024-09-25 16:16:13 +0200

    chore: Add experiments Assembly of rnns

--- experiments/rnn-of-rnns/main.py
+++ experiments/rnn-of-rnns/main.py
@@ -3,24 +3,44 @@ from ray import tune
 from ray import train
 from ray.tune import stopper
 
-from .invalid_stopper import InvalidStopper
-from .search_space import search_space
-from .trainable import RNNofRNNsTrainable
+from ray.tune import CLIReporter
+
+# from invalid_stopper import InvalidStopper
+from search_space import search_space
+from trainable import RNNofRNNsTrainable
 
 
 def main():
     epoch_stopper = stopper.MaximumIterationStopper(200)
     trial_plateau = stopper.TrialPlateauStopper(
-        metric="test_loss",
-        float=0.01,
-        num_results=10,
+        metric="test_acc",
+        std=0.001,
+        num_results=5,
         grace_period=50,
-        mode="min",
     )
-    invalid_stopper = InvalidStopper("test_acc", -1.0)
-    stoppers = stopper.CombinedStopper(epoch_stopper, trial_plateau, invalid_stopper)
+    # invalid_stopper = InvalidStopper("test_acc", -1.0)
+    stoppers = stopper.CombinedStopper(
+        epoch_stopper, trial_plateau
+    )  # , invalid_stopper)
+
+    reporter = CLIReporter(
+        parameter_columns={
+            "permute_seed": "seed",
+            "generalized_coupling": "gen_coupling",
+            "activation": "act",
+            "adapt_blocks": "adapt",
+            "squash_blocks": "squash",
+            "orthogonal_blocks": "orthogonal",
+        },
+        metric_columns=[
+            "train_loss",
+            "train_acc",
+            "test_loss",
+            "test_acc",
+        ],
+    )
     run_config = train.RunConfig(
-        storage_path=os.path.join(os.getcwd(), "experiments"),
+        storage_path=os.path.join(os.getcwd(), "results"),
         checkpoint_config=train.CheckpointConfig(
             num_to_keep=1,
             checkpoint_score_attribute="test_acc",
@@ -28,14 +48,12 @@ def main():
             checkpoint_frequency=1,
         ),
         stop=stoppers,
+        progress_reporter=reporter,
     )
 
-    tune_config = tune.TuneConfig(
-        num_samples=5,
-        reuse_actors=True,
-    )
+    tune_config = tune.TuneConfig(num_samples=3)
     tuner = tune.Tuner(
-        tune.with_resources(RNNofRNNsTrainable, {"cpu": 1, "gpu": 0.3}),
+        tune.with_resources(RNNofRNNsTrainable, {"cpu": 1, "gpu": 0.5}),
         param_space=search_space(),
         tune_config=tune_config,
         run_config=run_config,
--- experiments/rnn-of-rnns/utils.py
+++ experiments/rnn-of-rnns/utils.py
@@ -1,13 +1,12 @@
-import torch
 from torch.utils.data import DataLoader
 
 from torchrc.data.utils.seq_loader import seq_collate_fn
 from torchrc.data.datasets.seq_mnist import SequentialMNIST
 
 
-def load_mnist(train_batch_size: int, test_batch_size: int):
-    train_data = SequentialMNIST("./data", train=True, download=True)
-    test_data = SequentialMNIST("./data", train=False, download=True)
+def load_mnist(train_batch_size: int, test_batch_size: int, **kwargs):
+    train_data = SequentialMNIST(train=True, download=True, **kwargs)
+    test_data = SequentialMNIST(train=False, download=True, **kwargs)
 
     train_loader = DataLoader(
         train_data,
