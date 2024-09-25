005e1b07eb3421b74a34dcb6bf9022762cd36090
vdecaro <43961074+vdecaro@users.noreply.github.com>
2024-09-25 15:51:10 +0200

    feat: Add assembly of rnns

---
+++ torchrc/models/rnn_assembly/__init__.py
@@ -0,0 +1,5 @@
+from .block_diagonal import BlockDiagonal
+from .skew_symm_coupling import SkewAntisymmetricCoupling
+from .rnn_assembly import RNNAssembly
+
+__all__ = ["BlockDiagonal", "SkewAntisymmetricCoupling", "RNNAssembly"]
